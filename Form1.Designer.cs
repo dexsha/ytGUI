1569ec07446c33d9f517c05c34fd08621ab9746f
dexsha <shadex.forums@gmail.com>
2020-04-28 19:27:12 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace pyGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQuality = new System.Windows.Forms.TextBox();
+            this.pnlThreads = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowThreads = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlThreads.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(55, 38);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(65, 13);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "OutputLabel";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(55, 61);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(63, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "StatusLabel";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(96, 86);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(58, 10);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(158, 20);
+            this.txtUrl.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "URL:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Output:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Status:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 86);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Quality:";
+            // 
+            // txtQuality
+            // 
+            this.txtQuality.Location = new System.Drawing.Point(273, 10);
+            this.txtQuality.Name = "txtQuality";
+            this.txtQuality.Size = new System.Drawing.Size(24, 20);
+            this.txtQuality.TabIndex = 9;
+            // 
+            // pnlThreads
+            // 
+            this.pnlThreads.Controls.Add(this.flowThreads);
+            this.pnlThreads.Location = new System.Drawing.Point(330, 8);
+            this.pnlThreads.Name = "pnlThreads";
+            this.pnlThreads.Size = new System.Drawing.Size(311, 212);
+            this.pnlThreads.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // flowThreads
+            // 
+            this.flowThreads.Location = new System.Drawing.Point(3, 5);
+            this.flowThreads.Name = "flowThreads";
+            this.flowThreads.Size = new System.Drawing.Size(254, 173);
+            this.flowThreads.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 232);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pnlThreads);
+            this.Controls.Add(this.txtQuality);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.Text = "ytGUI";
+            this.pnlThreads.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQuality;
+        private System.Windows.Forms.Panel pnlThreads;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowThreads;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pyGUI
+{
+    public partial class Form1 : Form
+    {
+        public static Process ytdl = new Process();
+
+        public Form1()
+        {
+            InitializeComponent();        
+        }
+        public void StartProcess()
+        {
+            try
+            {
+                ytdl.StartInfo.FileName = "C:\\xampp\\htdocs\\pyGUI\\youtube-dl.exe";
+                ytdl.StartInfo.Arguments = "-o \"C:\\xampp\\htdocs\\pyGUI\\dl\\%(title)s %(timestamp)s\" -f " + txtQuality.Text + " " + txtUrl.Text;
+                ytdl.StartInfo.UseShellExecute = false;
+                ytdl.StartInfo.RedirectStandardOutput = true;
+                ytdl.StartInfo.CreateNoWindow = true;
+                ytdl.OutputDataReceived += (sender, e) => OutputHandler(lblOutput, e.Data);
+                ytdl.Start();
+                ytdl.BeginOutputReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Process error.");
+            }
+        }
+
+        private void OutputHandler(Label label, string text)
+        {
+            if (label.InvokeRequired)
+            {
+                label.Invoke((System.Action)(() => OutputHandler(label, text)));
+            }
+            else
+            {
+                label.Text = text;
+                if (text == "") {
+                    lblStatus.Text = "Stopped";
+                }
+                else if (text != "") {
+                    lblStatus.Text = "Running";
+                }
+            }
+            Console.WriteLine(text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ytdl.CloseMainWindow();
+            }
+            catch
+            {
+                Console.WriteLine("No process running.");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartProcess();
+            
+        }
+
+        private void CreatePanel()
+        {
+            Panel pnlWorker = new Panel();
+            pnlWorker.Width = pnlThreads.Width;
+            pnlWorker.Height = 25;
+            flowThreads.Controls.Add(pnlWorker);
+
+            Label lblWorkerUrl = new Label();
+            lblWorkerUrl.Top = 0;
+            lblWorkerUrl.Left = 0;
+
+            string[] channelName = txtUrl.Text.Split('/');
+
+            lblWorkerUrl.Text = channelName[channelName.Length-2];
+            pnlWorker.Controls.Add(lblWorkerUrl);
+
+            Button btnWorkerStop = new Button();
+            btnWorkerStop.Width = 50;
+            btnWorkerStop.Height = 25;
+            btnWorkerStop.Location = new Point(lblWorkerUrl.Width + 10, lblWorkerUrl.Location.Y);
+            btnWorkerStop.Text = "Stop";
+            pnlWorker.Controls.Add(btnWorkerStop);
+            //btnWorkerStop.Click += new EventHandler(StopThread);
+
+            //Label lblWorkerStatus = new Label();
+            //lblWorkerStatus.Top = 0;
+            //lblWorkerStatus.Left = lblWorkerUrl.Width + 10;
+            //lblWorkerStatus.Text = txtUrl.Text;
+            //pnlWorker.Controls.Add(lblWorkerUrl);
+        }
+
+        void StopThread(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            CreatePanel();
+        }
+    }
+}
